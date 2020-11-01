91d6a976a561fa2dadb369175af7e08d0d3a68f7
rizfa-is <67631569+rizfa-is@users.noreply.github.com>
2020-11-02 01:45:21 +0700

    Submission 1 Review : 1. Add default value for nullable POJO's class. 2. Add Activity keyword on SplashScreen activity. 3. Add mainlooper as Handler contructor to prevent deprecated.
    Feature :
    1. SplashScreen.
    2. No internet warning.

--- app/src/main/java/com/istekno/githubredesign/data/Developer.kt
+++ app/src/main/java/com/istekno/githubredesign/data/Developer.kt
@@ -5,19 +5,19 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Developer (
-    val name : String?,
-    val username : String?,
-    val location : String?,
-    val company : String?,
-    val avatar : String?,
-    val repository : Int?,
-    val follower : Int?,
-    val following : Int?
+    val name : String? = null,
+    val username : String? = null,
+    val location : String? = null,
+    val company : String? = null,
+    val avatar : String? = null,
+    val repository : Int? = null,
+    val follower : Int? = null,
+    val following : Int? = null
 ) : Parcelable
 
 @Parcelize
 data class Content (
-    val name : String?,
-    val photo : String?
+    val name : String? = null,
+    val photo : String? = null
 ) : Parcelable
 
--- app/src/main/java/com/istekno/githubredesign/features/SplashScreenActivity.kt
+++ app/src/main/java/com/istekno/githubredesign/features/SplashScreenActivity.kt
@@ -8,14 +8,14 @@ import android.view.WindowManager
 import com.istekno.githubredesign.R
 import com.istekno.githubredesign.activity.MainActivity
 
-class SplashScreen : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
 
-        Handler().postDelayed(
+        Handler(mainLooper).postDelayed(
             {
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
